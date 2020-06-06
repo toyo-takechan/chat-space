8b100a5897be26a3957e24ad15b8e16847adbb2d
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-06-07 08:50:10 +0900

    自動更新2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,7 +31,7 @@ $(function () {
             </div>
           </div>
           <div class="message__review">
-            <p class="message.__review__content">
+            <p class="message__review__content">
               ${message.content}
               </p>
           </div>
@@ -64,4 +64,78 @@ $(function () {
       $('.submit-btn').prop('disabled', false);
     })
   })
+  function buildHTML(message) {
+    if (message.image) {
+      //data-idが反映されるようにしている
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+            <div class="message__line">
+              <div class="message__line"__user-name">
+                ${message.user_name}
+              </div>
+              <div class="message__line"__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__review">
+              <p class="message__review__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+      return html;
+    } else {
+      //同様にdata-idが反映されるようにしている
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+            <div class="message__line">
+              <div class="message__line"__user-name">
+                ${message.user_name}
+              </div>
+              <div class="message__line"__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__review">
+              <p class="message__review__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+      return html;
+    };
+  }
+  var reloadMessages = function () {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: { id: last_message_id }
+    })
+      .done(function (messages) {
+        if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function (i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+        }
+      })
+      .fail(function () {
+        alert('error');
+      });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  # :indexを追記
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+#追加
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
   end
 end
+
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root 'groups#index'
+#   # :indexを追記
+#   resources :users, only: [:index, :edit, :update]
+#   resources :groups, only: [:new, :create, :edit, :update] do
+#     resources :messages, only: [:index, :create]
+#     namespace :api do
+#       resources :messages, only: :index, defaults: { format: 'json' }
+#     end
+#   end
+# end
 
 
 
