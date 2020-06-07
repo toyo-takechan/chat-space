973ba89a7b7265acf63110dc944922876a0cde21
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-06-07 10:33:47 +0900

    Update routes.rb
    routes.rbのコメントを削除

--- config/routes.rb
+++ config/routes.rb
@@ -12,31 +12,3 @@ Rails.application.routes.draw do
 end
 
 
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root 'groups#index'
-#   # :indexを追記
-#   resources :users, only: [:index, :edit, :update]
-#   resources :groups, only: [:new, :create, :edit, :update] do
-#     resources :messages, only: [:index, :create]
-#     namespace :api do
-#       resources :messages, only: :index, defaults: { format: 'json' }
-#     end
-#   end
-# end
-
-
-
-
-
-
-
-
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root 'groups#index'
-#   resources :users, only: [:index, :edit, :update]
-#   resources :groups, only: [:new, :create, :edit, :update] do
-#     resources :messages, only: [:index, :create]
-#   end
-# end
