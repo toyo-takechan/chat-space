e8a95ac16cd0838019489d80e38a3a45e6b143b2
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2020-06-07 10:32:02 +0900

    自動更新3

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,71 +1,6 @@
 $(function () {
   function buildHTML(message) {
     if (message.image) {
-      var html =`
-        <div class="message">
-          <div class="message__line">
-            <div class="message__line__name">
-              ${message.user_name}
-            </div>
-            <div class="message__line__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="message__review">
-            <p class="message__review__content">
-              ${message.content}
-            </p>
-            <img src=${message.image} >
-          </div>
-        </div>`
-      return html;
-    } else {
-      var html =`
-        <div class="message">
-          <div class="message__line">
-            <div class="message__line__name">
-              ${message.user_name}
-            </div>
-            <div class="message__line__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="message__review">
-            <p class="message__review__content">
-              ${message.content}
-              </p>
-          </div>
-        </div>`
-      return html;
-    };
-  }
-  $('#new_message').on('submit', function (e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function (data) {
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-    })
-    .fail(function () {
-      alert("メッセージ送信に失敗しました");
-    })
-    .always(function () {
-      $('form')[0].reset();
-      $('.submit-btn').prop('disabled', false);
-    })
-  })
-  function buildHTML(message) {
-    if (message.image) {
       //data-idが反映されるようにしている
       var html =
         `<div class="message" data-message-id=${message.id}>
@@ -106,6 +41,32 @@ $(function () {
       return html;
     };
   }
+  $('#new_message').on('submit', function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function (data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+    })
+    .fail(function () {
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function () {
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+  })
+  
   var reloadMessages = function () {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').data("message-id");
